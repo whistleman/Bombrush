7a03dc14953efac92af2b73313c500df97064224
Jacco Haasnoot <jacco.h@live.nl>
2021-01-11 12:08:26 +0100

    added AIS revive

---
+++ Files/AIS/Damage/cfgFunctions.hpp
@@ -0,0 +1,12 @@
+class AIS_Damage
+{
+    class AIS_Damage
+    {
+        file = "Files\AIS\Damage";
+		class exitDamageHandler;
+		class getHitIndexValue;
+		class goToDead;
+		class handleDamage;
+		class verifyDamageType;
+    };
+};
